5e7d57df2e5dc3904da444871f12c34b1a617af1
Carlosss2 <147357485+Carlosss2@users.noreply.github.com>
2024-09-24 18:53:15 -0600

    v.ooo

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  constructor(private router: Router){
 
+  }
+  ruta(){
+    this.router.navigate(["/home"])
+  }
 }
